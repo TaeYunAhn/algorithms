b1264657b996c585f18aa54402a8a58d56e62064
TaeYunAhn <80814896+TaeYunAhn@users.noreply.github.com>
2025-03-18 10:14:12 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5804 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,25 +1,22 @@
-#include <iostream>
 #include <algorithm>
+#include <iostream>
+
 using namespace std;
 
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
-    int N;
+int main()
+{
+    int N = 0;
+    int dp[1000001];
     cin >> N;
-    
-    int dp[1000001]; // N이 최대 10^6이므로
+    dp[0] = 0;
     dp[1] = 0;
-    
-    for (int i = 2; i <= N; i++){
-        dp[i] = dp[i - 1] + 1; // 1을 빼는 경우
-        if (i % 2 == 0)
-            dp[i] = min(dp[i], dp[i / 2] + 1);
-        if (i % 3 == 0)
-            dp[i] = min(dp[i], dp[i / 3] + 1);
+    dp[2] = 1;
+    dp[3] = 1;
+    for(int i = 4; i <= N; i++){
+        dp[i] = dp[i-1] + 1;
+        if(i % 2 == 0) dp[i] = min(dp[i], dp[i/2] + 1);
+        if(i % 3 == 0) dp[i] = min(dp[i], dp[i/3] + 1);
     }
-    
-    cout << dp[N] << "\n";
+    cout << dp[N];
     return 0;
 }
